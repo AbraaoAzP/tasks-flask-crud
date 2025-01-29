b4863d92e5b49947155634e5edaa3507e50ce415
AbraaoAzP <abraaoazp@gmail.com>
2025-01-28 21:37:33 -0300

    Add get_tasks & get_task_by_id

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ task_id_control = 1
 def create_task():
     global task_id_control
     # global vai permitir que a variável criada fora do escopo seja reconhecida.
+    # Sempre que formos utilizar uma variável e vamos realizar uma iteração com ela dentro do método, teremos que usar o Global.
     data = request.get_json()
     # request: vai ser utilizado para recuperar as informações que precisam ser passadas.
     # get_json(): vai recuperar o que o cliente enviou.
@@ -21,6 +22,27 @@ def create_task():
     print(tasks)
     return jsonify({"message": "Nova tarefa criada com sucesso!"})
     # vai retornar a mensagem no formato json, graças ao jsonify.
+
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    task_list = [task.to_dict() for task in tasks]
+    # Cria uma nova lista e dentro um for está sendo executado iterando sobre todos os elementos da lista task e retornando a cada iteração o task.to_dict().
+
+    # for task in tasks:
+    #     task_list.append(task.to_dict())
+    output = {
+        "tasks": task_list,
+        "total_tasks": len(task_list)
+    }
+    return jsonify(output)
+
+@app.route('/tasks/<int:id>', methods=['GET'])
+def get_task_by_id(id):
+    for t in tasks:
+        if t.id == id:
+            return jsonify(t.to_dict())
+        
+    return jsonify({"message": "Não foi possível encontrar a atividade."}), 404
 
 if __name__ == "__main__":
     app.run(debug=True)
